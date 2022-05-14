3a07e6ea8bbed8a5de38aa14c30b682e5a3deef0
CamillaSimicich <100423042+CamillaSimicich@users.noreply.github.com>
2022-05-14 22:44:31 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,34 @@
+#include "lib.h"
 
+
+void init (char x[][20], int a){
+    for (int i=0; i<a; i++){
+        for (int j=0; j<20; j++){
+            x[i][j]=0;
+        }
+    }
+}
+
+void init (char x[], int a){
+    for (int i=0; i<a; i++){
+        x[i]=0;
+    }
+
+}
+
+int cerca (char elenco_nomi [10][20], char nome [20]){
+    int c;
+    for (int i=0; i<10; i++) {
+        c = 0;
+        for (int j = 0; j < 20; j++) {
+            if (elenco_nomi[i][j] == nome[j]) {
+                c++;
+            } else
+                break;
+        }
+        if (c == 20) {
+            return i;
+        }
+    }
+    return -1;
+}
