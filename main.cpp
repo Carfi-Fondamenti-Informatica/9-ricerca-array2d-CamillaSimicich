d09c1d9ada9311d35aa6e4b36c8614bd1ff3a308
CamillaSimicich <100423042+CamillaSimicich@users.noreply.github.com>
2022-05-14 22:45:35 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+int main (){
+        char elenco_nomi [10][20];
+        char nome [20];
+        init (elenco_nomi, 10);
+        init (nome, 20);
+        for (int i=0; i<10; i++){
+            cin>>elenco_nomi[i];
+        }
+        cin>> nome;
+        int z= cerca (elenco_nomi, nome);
+        if (z==-1){
+            cout<<"non presente"<<endl;
+        } else {
+            cout<<z<<endl;
+        }
 
-int main(){
-  return 0;
-}
+        return 0;
+    }
