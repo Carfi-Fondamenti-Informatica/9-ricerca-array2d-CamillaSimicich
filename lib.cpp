051e52c7f64cdaf69d8358a94ba8beeb061bb749
CamillaSimicich <100423042+CamillaSimicich@users.noreply.github.com>
2022-05-14 22:56:11 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -16,12 +16,12 @@ void init (char x[], int a){
 
 }
 
-int ricerca (char elenco_nomi [10][20], char nome [20]){
+int ricerca (char lista_nomi [10][20], char nome [20]){
     int c;
     for (int i=0; i<10; i++) {
         c = 0;
         for (int j = 0; j < 20; j++) {
-            if (elenco_nomi[i][j] == nome[j]) {
+            if (lista_nomi[i][j] == nome[j]) {
                 c++;
             } else 
                 break;
